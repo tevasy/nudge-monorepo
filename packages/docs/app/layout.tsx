e0c29dd735e658b78006fee48b99d3ccbaf9d90e
tevasy <tatianavasy01@gmail.com>
2025-03-13 21:17:35 +0100

    fixing nudge-library imports

--- packages/docs/app/layout.tsx
+++ packages/docs/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       setIsLoaded(true);
       setTimeout(() => {
         setShowSpinner(false);
-      }, 15000);
+      }, 25000);
     };
 
     if (document.readyState === "complete") {
